683e00707f5cb5404ca9199b1a62ee4c75d38a70
RudyLo <63445186+RudyLo@users.noreply.github.com>
2020-12-20 14:22:05 +0100

    [collision] Modélisation des quilles avec leur numéro respectif + ajout de rigidbody pour la collision

---
+++ Projet_X_Reality_Molky_IA2/Assets/MoveCapsule.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCapsule : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float force = 3f;
+    bool lance = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lance == false)
+        {
+            Vector3 position = transform.position;
+            float axeX = Input.GetAxis("Horizontal");
+            position.z = axeX/4;
+
+            transform.position = position;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                lance = true;
+                GetComponent<Rigidbody>().AddForce(transform.forward * force);
+            }
+        }
+    }
+}
